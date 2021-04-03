b89cf0bc07b1387de3e585a8d626e830dda0d4f4
Anh7999 <55305554+Anh7999@users.noreply.github.com>
2021-04-03 09:24:13 +0700

    new

---
+++ TMDT/Controllers/Usedao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using TMDT.Models;
+namespace TMDT.Controllers
+{
+    public class Usedao
+    {
+        DoAnTMDTEntities db = null;
+
+        public Usedao()
+        {
+            db = new DoAnTMDTEntities();
+        }
+        public long Insert(Account entity)
+        {
+           
+                db.Accounts.Add(entity);
+                db.SaveChanges();
+                return entity.Sdt;
+
+
+        }
+               
+              
+    public bool CheckUserName(string tenKH)
+        {
+            return db.Accounts.Count(x => x.TenKH == tenKH) > 0;
+        }
+      
+    }
+}
---
+++ TMDT/Controllers/dangkykhachhangdao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TMDT.Models;
+
+namespace TMDT.Controllers
+{
+    public class dangkykhachhangdao
+    {
+
+
+        DoAnTMDTEntities db = null;
+
+        public dangkykhachhangdao()
+        {
+            db = new DoAnTMDTEntities();
+        }
+        public long Insert(taikhoangkhachhang entity)
+        {
+
+            db.taikhoangkhachhangs.Add(entity);
+            db.SaveChanges();
+            return entity.id;
+
+
+        }
+
+
+     
+        public bool CheckUserName(string tennn)
+        {
+            return db.taikhoangkhachhangs.Count(x => x.Ten == tennn) > 0;
+        }
+        public bool checkgmail(string gmail)
+        {
+            return db.taikhoangkhachhangs.Count(x => x.Email == gmail) > 0;
+        }
+
+    }
+}
